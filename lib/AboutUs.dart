be6f841826dc07b5e90ec88a29372b415723bac4
cesarRz <cesarrz2000@gmail.com>
2022-09-25 14:29:23 -0500

    load image

--- lib/AboutUs.dart
+++ lib/AboutUs.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 class AboutUs extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-            title: const Text('Nosotros'),
-            backgroundColor: Color.fromARGB(255, 0, 0, 0)),
-        body: const Center(
-            child: Text('Nosotros', style: TextStyle(fontSize: 60))),
-      );
+      appBar: AppBar(
+          title: const Text('Nosotros'),
+          backgroundColor: Color.fromARGB(255, 0, 0, 0)),
+      body: const Image(image: AssetImage('assets/images/restaurante1.jpg')));
 }
