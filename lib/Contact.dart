11268da889c0d482bcbb7cd26d14cc6977a48df3
cesarRz <cesarrz2000@gmail.com>
2022-09-25 16:02:47 -0500

    Whats app Contact

--- lib/Contact.dart
+++ lib/Contact.dart
@@ -1,12 +1,79 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_sms/flutter_sms.dart';
 
-class Contact extends StatelessWidget {
+// class Contactanos extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) => Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Contactanos'),
+//           backgroundColor: Colors.black
+//         ),
+//         body: const Center(child: Text('Feed', style: TextStyle(fontSize: 60))),
+//       );
+// }
+
+class Contact extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-            title: const Text('Contacto'),
-            backgroundColor: Color.fromARGB(255, 0, 0, 0)),
-        body: const Center(
-            child: Text('Contacto', style: TextStyle(fontSize: 60))),
-      );
+  _ContactState createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  openwhatsapp(String message, String destinatario) async {
+    var whatsapp = destinatario;
+    var whatsappURl_android =
+        "whatsapp://send?phone=" + whatsapp + "&text=" + message;
+    var whatappURL_ios = "https://wa.me/$whatsapp?text=${Uri.parse(message)}";
+    if (Platform.isIOS) {
+      // for iOS phone only
+      if (await canLaunch(whatappURL_ios)) {
+        await launch(whatappURL_ios, forceSafariVC: false);
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: new Text("whatsapp no instalado")));
+      }
+    } else {
+      // android , web
+      if (await canLaunch(whatsappURl_android)) {
+        await launch(whatsappURl_android);
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: new Text("whatsapp no installed")));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Comunicaciones Flutter'),
+          ),
+          body: Center(
+              child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(25),
+              width: 100,
+              child: TextButton(
+                child: Text(
+                  'Whatsapp',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal,
+                  onPrimary: Colors.white,
+                  onSurface: Colors.grey,
+                ),
+                onPressed: () {
+                  String mensaje = "Esto es un mensaje";
+                  String destinatario = "+52555555555";
+                  openwhatsapp(mensaje, destinatario);
+                },
+              ),
+            )
+          ]))),
+    );
+  }
 }
